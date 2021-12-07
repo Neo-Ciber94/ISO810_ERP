516c74a4d017a528c9d28256c13d2c25657a2f54
Freddy L <neo-ciber_94@hotmail.com>
2021-12-06 22:51:18 -0400

    Updated Organization model

--- backend/Models/Organization.cs
+++ backend/Models/Organization.cs
@@ -1,16 +1,18 @@
 
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace ISO810_ERP.Models;
 
+[Index(nameof(Account), IsUnique = true)]
 public class Organization
 {
     public int Id { get; set; }
-    public int Account { get; set; }
+    public long Account { get; set; }
     public string Name { get; set; } = default!;
     public string Alias { get; set; } = default!;
-    
+
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public DateTime CreatedAt { get; set; }
 
