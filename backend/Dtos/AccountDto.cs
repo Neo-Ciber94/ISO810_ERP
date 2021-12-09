f6de582a4f2a2c73c76c62555e1672bcaa53f124
Freddy L <neo-ciber_94@hotmail.com>
2021-12-08 20:07:05 -0400

    Updated Dto

--- backend/Dtos/AccountDto.cs
+++ backend/Dtos/AccountDto.cs
@@ -1,8 +1,13 @@
+using System;
+
 namespace ISO810_ERP.Dtos;
 
-    public class AccountDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = default!;
-        public string Email { get; set; } = default!;
-    }
+public class AccountDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+}
